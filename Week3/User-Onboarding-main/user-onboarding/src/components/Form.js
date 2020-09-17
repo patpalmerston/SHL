ffe07fbc123ead3171c65489e2c9e907aa23485a
Pat Palmerston <patpalmerston@gmail.com>
2020-09-17 09:01:25 -0700

    updated

---
+++ Week3/User-Onboarding-main/user-onboarding/src/components/Form.js
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import * as Yup from 'yup';
+
+const Form = (props) => {
+    const [formState, setFormState] = useState({
+        name: '',
+        email: '',
+        password: '',
+        terms: '',
+    });
+
+    const [errors, setErrors] = useState({
+        name: '',
+        email: '',
+        password: '',
+    });
+
+    const [isButtonDisabled, setIsButtonDisabled] = useState(true);
+
+    console.log('form.js props', props);
+    return (
+        <div>
+            <form />
+            <label>
+                Name:
+                <br></br>
+                <input type='text' value='name' />
+            </label>
+            <label>
+                Name:
+                <br></br>
+                <input type='text' value='name' />
+            </label>
+            <label>
+                Name:
+                <br></br>
+                <input type='text' value='name' />
+            </label>
+            <label>
+                Name:
+                <br></br>
+                <input type='text' value='name' />
+            </label>
+            <label>
+                Name:
+                <br></br>
+                <input type='text' value='name' />
+            </label>
+        </div>
+    );
+};
+
+export default Form;
